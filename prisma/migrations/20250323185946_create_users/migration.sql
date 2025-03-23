cfbdfdca79e85f43e80e98c9bcb3652e69e0a0e1
José Victor <josevictornascimento2016@gmail.com>
2025-03-23 16:01:04 -0300

    chore: :card_file_box: configured prisma setup

---
+++ prisma/migrations/20250323185946_create_users/migration.sql
@@ -0,0 +1,15 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('ADMIN', 'SELLER');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'SELLER',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
